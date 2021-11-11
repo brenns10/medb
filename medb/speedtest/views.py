1b7d80892a291ea22b1474f7f96fb9e55e83a455
Stephen Brennan <stephen@brennan.io>
2021-11-10 18:12:49 -0800

    speedtest: fix time zone display in chart

--- medb/speedtest/views.py
+++ medb/speedtest/views.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 """Views for speed testing"""
+import datetime
+
 import mpld3
 import pandas as pd
 from flask import Blueprint
@@ -23,6 +25,14 @@ blueprint = Blueprint(
 @login_required
 def speedtest_results():
     df = pd.read_sql(SpeedTestResult.query.statement, db.session.bind)
+    local_tz = datetime.datetime.now().astimezone().tzinfo
+    # This bizarre line is due to the following:
+    # 1. We store times in the database as UTC, but want to present them in the
+    #    local timezone.
+    # 2. Pandas somehow doesn't support timezone aware datetimes
+    # So, we convert from UTC to the local time, and then strip the timezone off
+    # of it. This results in the timezone "looking" correct in the plot.
+    df["time"] = df["time"].dt.tz_convert(local_tz).dt.tz_localize(None)
     df = df.set_index("time")
     df["Upload (Mbps)"] = df["upload_bps"] / 1000000.0
     df["Download (Mbps)"] = df["download_bps"] / 1000000.0
